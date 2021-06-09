8c09f4c2434fe454265cf2fdb287e72f7abbaab9
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-06-09 13:56:51 +0700

    fix: Temporary disable RecyclerView adapter

--- app/src/main/java/io/taskoverflow/adapter/TasksAdapter.java
+++ app/src/main/java/io/taskoverflow/adapter/TasksAdapter.java
@@ -1,9 +1,12 @@
 package io.taskoverflow.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,46 +16,53 @@ import java.util.ArrayList;
 
 import io.taskoverflow.R;
 import io.taskoverflow.Task;
+import io.taskoverflow.database.DatabaseOpenHelper;
 
-public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
-    ArrayList<Task> tasks;
-    Context ctx;
-
-    public static class TasksViewHolder extends RecyclerView.ViewHolder {
-        private final TextView transactionIdTV, productIdTV;
-
-        public TasksViewHolder(View view) {
-            super(view);
-//            transactionIdTV = view.findViewById(R.id.transaction_id);
-//            productIdTV = view.findViewById(R.id.product_id);
-        }
-
-        public TextView getTransactionIdTV() { return transactionIdTV; }
-
-        public TextView getProductIdTV() { return productIdTV; }
-    }
-
-    // Public constructor
-    /**
-     * Creates a new RecyclerViewAdapter based on the given ArrayList.
-     */
-    public TasksAdapter(Context ctx) { this.ctx = ctx; }
-
-    public void setTransactions(ArrayList<Task> tasks) { this.tasks = tasks; }
-
-    @Override
-    public int getItemCount() { return tasks.size(); }
-
-    @NonNull
-    @Override
-    public TasksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.transaction_item, parent, false);
-//        return new TasksViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull TasksAdapter.TasksViewHolder holder, int position) {
-//        holder.getTransactionIdTV().setText(String.valueOf(tasks.get(position).id));
-//        holder.getProductIdTV().setText(String.valueOf(tasks.get(position).getProductId()));
+public class TasksAdapter {
+    TasksAdapter(){
+        int i = 0;
     }
 }
+
+// public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
+//     private Context ctx;
+//     private SQLiteDatabase db;
+//     private Cursor cursor;
+//     private int total;
+//
+//     public TasksAdapter(Context ctx){
+//         this.ctx = ctx;
+//         this.db = new DatabaseOpenHelper(ctx).getReadableDatabase();
+//         this.cursor = db.query(DatabaseOpenHelper.DATABASE_TABLE_TASKS, null, null, null, null, null, null);
+//         this.total = cursor.getCount();
+//     }
+//
+//     public static class TasksViewHolder extends RecyclerView.ViewHolder {
+//         private final LinearLayout expenseItemContainer;
+//         private final TextView expenseNameTV, expenseNominalTV, expenseDateTV;
+//
+//         public TasksViewHolder(@NonNull View view) {
+//             super(view);
+//             expenseItemContainer = view.findViewById(R.id.expense_item_container);
+//             expenseNameTV = view.findViewById(R.id.expense_name);
+//             expenseNominalTV = view.findViewById(R.id.expense_nominal);
+//             expenseDateTV = view.findViewById(R.id.expense_date);
+//         }
+//
+//         public LinearLayout getExpenseItemContainer() { return expenseItemContainer; }
+//
+//         public TextView getExpenseNameTV() { return expenseNameTV; }
+//
+//         public TextView getExpenseNominalTV() { return expenseNominalTV; }
+//
+//         public TextView getExpenseDateTV() { return expenseDateTV; }
+//     }
+//
+//     @Override
+//     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull TasksAdapter.TasksViewHolder holder, int position) {
+//
+//     }
+//
+//     @Override
+//     public int getItemCount() { return total; }
+// }
